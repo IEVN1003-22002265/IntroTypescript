256411942d200d4a31f5f0e1ab06e42d20a22ce5
DiegoG <81668@alumnos.utleon.edu.mx>
2026-09-04 23:47:28 -0600

    Tipos de datos 04-09-2026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+var num1 = 23;
+let num2 = 65;
+const num3 = 22;
+
+let nombre:string;
+let num:number;
+let activo:boolean;
+let sum:number=num1+num2+num3;
+
+console.log("la suma es: ",sum);
+
+
+console.log(nombre);
+console.log("Prueba");
+
